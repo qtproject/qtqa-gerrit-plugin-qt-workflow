c0b9476bf18ef955f0a3b81bd9d6f4fc5f02f4aa
Jukka Jokiniva <jukka.jokiniva@qt.io>
2020-04-03 13:51:32 +0300

    Add change-integration-pass and change-integration-fail events
    Task-numnber: QTQAINFRA-3621
    Change-Id: Iccdaabb4aa1a658a9c7fd8141ea231489cf6fb44
    Reviewed-by: Daniel Smith <Daniel.Smith@qt.io>

---
+++ src/main/java/com/googlesource/gerrit/plugins/qtcodereview/QtChangeIntegrationFailEvent.java
@@ -0,0 +1,20 @@
+//
+// Copyright (C) 2020 The Qt Company
+//
+
+package com.googlesource.gerrit.plugins.qtcodereview;
+
+import com.google.common.base.Supplier;
+import com.google.gerrit.reviewdb.client.Change;
+import com.google.gerrit.server.data.AccountAttribute;
+import com.google.gerrit.server.events.PatchSetEvent;
+
+public class QtChangeIntegrationFailEvent extends PatchSetEvent {
+  public static final String TYPE = "change-integration-fail";
+  public Supplier<AccountAttribute> submitter;
+  public String newRev;
+
+  public QtChangeIntegrationFailEvent(Change change) {
+    super(TYPE, change);
+  }
+}
